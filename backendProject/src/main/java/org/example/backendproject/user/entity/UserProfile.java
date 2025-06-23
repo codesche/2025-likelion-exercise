c49138e2167206f1e08e9a2ab6918042d87ac675
codesche <codesche@gmail.com>
2025-06-23 23:25:57 +0900

    UserProfile 생성

---
+++ backendProject/src/main/java/org/example/backendproject/user/entity/UserProfile.java
@@ -0,0 +1,42 @@
+package org.example.backendproject.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String address;
+
+    @OneToOne                       // 1:1 관계
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
