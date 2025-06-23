d5b046cacaad3839fd7f0dd460918e874c98b3a0
codesche <codesche@gmail.com>
2025-06-23 23:28:38 +0900

    getRoomId() -> getTo() 로 변경

--- backendProject/src/main/java/org/example/backendproject/stompwebsocket/controller/ChatController.java
+++ backendProject/src/main/java/org/example/backendproject/stompwebsocket/controller/ChatController.java
@@ -52,12 +52,12 @@ public class ChatController {
         if (message.getTo() != null && !message.getTo().isEmpty()) {
             // 귓속말
             // 내 아이디로 귓속말 경로를 활성화 함
-            channel = "private." + message.getRoomId();
+            channel = "private." + message.getTo();
             msg = objectMapper.writeValueAsString(message);
         } else {
             // 일반 메시지
             // message에서 roomId를 추출해서 해당 roomId를 구독하고 있는 클라이언트에게 메세지를 전달
-            channel = "room." + message.getRoomId();
+            channel = "room." + message.getTo();
             msg = objectMapper.writeValueAsString(message);
         }
 
