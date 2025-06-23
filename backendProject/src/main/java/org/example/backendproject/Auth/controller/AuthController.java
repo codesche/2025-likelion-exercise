2cd94ba81e60a948df285901b8de3b05aaf6920f
codesche <codesche@gmail.com>
2025-06-23 23:24:02 +0900

    Auth 컨트롤러 생성

---
+++ backendProject/src/main/java/org/example/backendproject/Auth/controller/AuthController.java
@@ -0,0 +1,52 @@
+package org.example.backendproject.Auth.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.backendproject.Auth.dto.LoginRequestDTO;
+import org.example.backendproject.Auth.dto.SignUpRequestDTO;
+import org.example.backendproject.Auth.service.AuthService;
+import org.example.backendproject.user.dto.UserDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/auth")
+@Slf4j
+public class AuthController {
+
+    private final AuthService authService;
+
+    /** 회원가입 **/
+    @PostMapping("/signUp")
+    public ResponseEntity<String> signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
+        try {
+            authService.signUp(signUpRequestDTO);
+            return ResponseEntity.ok("회원가입 성공");
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());     // 401
+        }
+    }
+
+    /** 로그인 **/
+    @PostMapping("/login")
+    public ResponseEntity<UserDTO> login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        try {
+            UserDTO loginUser = authService.login(loginRequestDTO);
+            log.info("로그인 성공 = " + new ObjectMapper().writeValueAsString(loginRequestDTO));
+            return ResponseEntity.ok(loginUser);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();     // 401
+        }
+    }
+
+
+}
