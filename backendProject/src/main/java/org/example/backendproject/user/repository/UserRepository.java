85d90826ff93e8b9d7fe95f3ca9d471779c417c4
codesche <codesche@gmail.com>
2025-06-23 23:26:03 +0900

    UserRepository 생성

---
+++ backendProject/src/main/java/org/example/backendproject/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.example.backendproject.user.repository;
+
+import java.util.Optional;
+import org.example.backendproject.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserid(String userid);
+
+}
