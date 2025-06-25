11fd5c8acdf16ba9cd0c7496eb2ccf34acd27f48
codesche <codesche@gmail.com>
2025-06-25 23:53:04 +0900

    feat(jwt): JwtKey 유틸 클래스 생성

---
+++ backendProject/src/main/java/org/example/backendproject/security/jwt/JwtKey.java
@@ -0,0 +1,24 @@
+package org.example.backendproject.security.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtKey {
+
+    @Value("${jwt.secretKey}")
+    private String secretKey;
+
+    // 서명키를 만들어서 반환하는 메서드
+    @Bean
+    public SecretKey secretKey() {
+        byte[] keyBytes = secretKey.getBytes();     // 설정파일에서 불러온 키 값을 바이트 배열로 변환
+
+        return Keys.hmacShaKeyFor(keyBytes);
+//        return new SecretKeySpec(keyBytes, "HmacSHA512");   // 바이트 배열을 HmacSHA256용 Security 객체로 변환
+    }
+
+}
