8653d4bc7c42a10fd77075e893a95c22ab6b5153
codesche <codesche@gmail.com>
2025-06-25 23:54:10 +0900

    feat(dto): 로그인 Response DTO 작성

---
+++ backendProject/src/main/java/org/example/backendproject/Auth/dto/LoginResponseDTO.java
@@ -0,0 +1,31 @@
+package org.example.backendproject.Auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.backendproject.Auth.entity.Auth;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseDTO {
+
+    private String tokenType;
+    private String accessToken;
+    private String refreshToken;
+    private Long userId;
+
+    @Builder
+    public LoginResponseDTO(Auth auth) {
+        this.tokenType = auth.getTokenType();
+        this.accessToken = auth.getAccessToken();
+        this.refreshToken = auth.getRefreshToken();
+        this.userId = auth.getId();
+    }
+
+
+
+}
