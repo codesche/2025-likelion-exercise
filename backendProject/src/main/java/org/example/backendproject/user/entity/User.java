45da99d2bd3b7bcbb6b89f41486b46ec65cf6240
codesche <codesche@gmail.com>
2025-06-25 23:55:56 +0900

    feat(entity): role 변수 추가

--- backendProject/src/main/java/org/example/backendproject/user/entity/User.java
+++ backendProject/src/main/java/org/example/backendproject/user/entity/User.java
@@ -3,6 +3,8 @@ package org.example.backendproject.user.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +19,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.example.backendproject.Auth.entity.Auth;
 import org.example.backendproject.board.entity.Board;
+import org.example.backendproject.security.core.Role;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -43,5 +46,8 @@ public class User {
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Board> boards = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)        // 이 필드를 DB에 문자열로 저장
+    private Role role;
 
 }
