336f51067cae830e5d9d5a8dd09252f5393c0201
codesche <codesche@gmail.com>
2025-06-25 23:43:52 +0900

    feat(repository): findByRefreshToken, existsByUser 메서드 추가

--- backendProject/src/main/java/org/example/backendproject/Auth/repository/AuthRepository.java
+++ backendProject/src/main/java/org/example/backendproject/Auth/repository/AuthRepository.java
@@ -1,12 +1,15 @@
 package org.example.backendproject.Auth.repository;
 
+import java.util.Optional;
 import org.example.backendproject.Auth.entity.Auth;
+import org.example.backendproject.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AuthRepository extends JpaRepository<Auth, Long> {
 
+    Optional<Auth> findByRefreshToken(String refreshToken);
 
-
+    boolean existsByUser(User user);
 }
