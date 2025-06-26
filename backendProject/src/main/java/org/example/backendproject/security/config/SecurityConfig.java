553728efdb443a45dca3d884865a5efeac4bf9d3
codesche <codesche@gmail.com>
2025-06-27 00:03:45 +0900

    feat(security): 소셜 로그인, 로그아웃, 웹소켓 핸드셰이크, 프로메테우스 모두 사용할 수 있게 해주는 설정 추가

--- backendProject/src/main/java/org/example/backendproject/security/config/SecurityConfig.java
+++ backendProject/src/main/java/org/example/backendproject/security/config/SecurityConfig.java
@@ -2,15 +2,22 @@ package org.example.backendproject.security.config;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.example.backendproject.oauth2.OAuth2LoginSuccessHandler;
+import org.example.backendproject.oauth2.OAuth2LogoutSuccessHandler;
+import org.example.backendproject.oauth2.OAuth2UserService;
+import org.example.backendproject.oauth2.RedisOAuth2AuthorizationRequestRepository;
 import org.example.backendproject.security.jwt.JwtTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -21,6 +28,16 @@ public class SecurityConfig {
 
     private final JwtTokenFilter jwtTokenFilter;
 
+    private final OAuth2LogoutSuccessHandler oAuth2LogoutSuccessHandler;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2UserService oAuth2UserService;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Bean
+    public AuthorizationRequestRepository<OAuth2AuthorizationRequest> authorizationRequestRepository() {
+        return new RedisOAuth2AuthorizationRequestRepository(redisTemplate);
+    }
+
     // 스프링 시큐리티에서 어떤 순서로 어떤 보안 규칙의 필터를 거칠지를 정의하는 클래스
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -30,11 +47,24 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/", "/index.html", "/*.html", "/favicon.ico",
-                    "/css/**", "fetchWithAuth.js", "/js/**", "/images/**",
-                   "/.well-know/**" ).permitAll()                       // 인증 필요없이 모두 허용
-                .requestMatchers("/boards/**", "/api/auth/**").permitAll()
-                .requestMatchers("/api/comments/**").permitAll()
-                .requestMatchers( "/api/user/**").authenticated()       // 인증이 필요한 경로
+                    "/css/**", "/fetchWithAuth.js", "/js/**", "/images/**",
+                   "/.well-known/**" ).permitAll()                       // 정적 리소스 누구나 접근
+                    .requestMatchers("/boards/**", "/api/comments/**").authenticated()
+
+                    //인증필요
+                    .requestMatchers(
+                        "/api/auth/**",       // 로그인/회원가입/로그아웃 등 인증 없이 사용
+//                        "/api/comments/**",   // 댓글 읽기 등 인증 없이 사용
+                        "/oauth2/**",         // 소셜 로그인 엔드포인트는 누구나 접근
+                        "/login/**",          // 스프링 시큐리티 내부 로그인 관련 엔드포인트
+                        "/ws-gpt", "/ws-chat", // 웹소켓 핸드셰이크
+                        "/actuator/prometheus" //프로메테우스
+                    ).permitAll() // 웹소켓 핸드셰이크는 모두 허용!
+
+                    .requestMatchers(
+                        "/api/user/**",
+                        "/api/rooms/**"
+                    ).authenticated() //인증이 필요한 경로
             )
             // 인증 실패시 예외 처리
             .exceptionHandling(e -> e
@@ -55,6 +85,21 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
             .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+
+            .oauth2Login(oauth2->oauth2
+                .loginPage("/index.html")
+                .userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+                .successHandler(oAuth2LoginSuccessHandler)
+
+                .authorizationEndpoint(authorization -> authorization
+                    .authorizationRequestRepository(authorizationRequestRepository()))
+            )
+            .logout(logout -> logout
+                .logoutUrl("/logout")
+                .logoutSuccessHandler(oAuth2LogoutSuccessHandler)
+                .permitAll()
+            )
+
             .build();           // 위 명시한 설정들을 적용
     }
 
