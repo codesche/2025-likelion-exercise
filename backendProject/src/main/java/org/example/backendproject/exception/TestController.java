748573ff339661ba82da7b6667b8a53ad5be4072
codesche <codesche@gmail.com>
2025-06-28 23:37:42 +0900

    update readme

---
+++ backendProject/src/main/java/org/example/backendproject/exception/TestController.java
@@ -0,0 +1,12 @@
+package org.example.backendproject.exception;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exception")
+public class TestController {
+
+
+
+}
