3fab6a47c97d3053efe21ee291e366909a7a2f18
codesche <codesche@gmail.com>
2025-07-02 00:04:10 +0900

    feat(repository): 조회수 칼럼 추가 및 생성일 기준으로 정렬

--- backendProject/src/main/java/org/example/backendproject/board/repository/BoardRepository.java
+++ backendProject/src/main/java/org/example/backendproject/board/repository/BoardRepository.java
@@ -25,7 +25,7 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     /** 검색기능 **/
     // 제목 또는 내용에 키워드가 포함된 글 검색 (대소문자 구분 없음)
     @Query("SELECT new org.example.backendproject.board.dto.BoardDTO(" +
-        "b.id, b.title, b.content,b.user.userProfile.username, b.user.id, b.created_date, b.updated_date" +
+        "b.id, b.title, b.content,b.user.userProfile.username, b.user.id, b.created_date, b.updated_date, b.viewCount" +
         ") " +
         "FROM Board b " +
         "WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
@@ -35,9 +35,9 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     /** 페이징 적용 후 **/
     //페이징 전체 목록
     @Query("SELECT new org.example.backendproject.board.dto.BoardDTO(" +
-        "b.id, b.title, b.content,b.user.userProfile.username, b.user.id,b.created_date, b.updated_date) " +
-        "FROM Board b")
-    //  + "ORDER BY b.title DESC") //쿼리로 정렬
+        "b.id, b.title, b.content,b.user.userProfile.username, b.user.id,b.created_date, b.updated_date, b.viewCount) " +
+        "FROM Board b "
+    + "ORDER BY b.created_date DESC") //쿼리로 정렬
     Page<BoardDTO> findAllPaging(Pageable pageable);
     //페이징 처리 결과를 담는 페이징 객체입니다.
     //전체 페이지수, 현재 페이지 번호,전체 아이템 겟수 등 페이징 관련 모든 정보들을 반환합니다.
@@ -47,11 +47,11 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     //페이징 검색 목록
     @Query("SELECT new org.example.backendproject.board.dto.BoardDTO(" +
-        "b.id, b.title, b.content,b.user.userProfile.username, b.user.id,  b.created_date, b.updated_date) " +
+        "b.id, b.title, b.content,b.user.userProfile.username, b.user.id,  b.created_date, b.updated_date, b.viewCount) " +
         "FROM Board b " +
         "WHERE LOWER(b.title) LIKE LOWER(CONCAT('%', :keyword, '%')) " +
-        "OR LOWER(b.content) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    // + "ORDER BY b.title DESC")// 쿼리로 정렬
+        "OR LOWER(b.content) LIKE LOWER(CONCAT('%', :keyword, '%'))"
+     + "ORDER BY b.created_date DESC")// 쿼리로 정렬
     Page<BoardDTO> searchKeywordPaging(@Param("keyword") String keyword, Pageable pageable);
 
 }
